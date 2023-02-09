e98870c21ea9360e7ae39d9f87d3eaa940cb6554
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2023-02-09 11:39:01 +0100

    divido la habitacion

---
+++ ejercicio.py
@@ -0,0 +1,13 @@
+#divido la habitacion en zonas
+
+zona1 = {"largo":500,"ancho":150}
+zona2 = {"largo":480,"ancho":101}
+zona3 = {"largo":309,"ancho":480}
+zona4 = {"largo":90,"ancho":220}
+
+#creo una lista con las zonas para tener la zona total
+zona_total = []
+zona_total.append(zona1)
+zona_total.append(zona2)
+zona_total.append(zona3)
+zona_total.append(zona4)
