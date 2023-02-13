fa5771ff1c1f5a0222c8c45bd6c7b461a5ef32e1
migueliiin <91721552+migueliiin@users.noreply.github.com>
2023-02-13 16:07:44 +0100

    VOLANDO

--- ejercicio.py
+++ ejercicio.py
@@ -1,35 +1,25 @@
 from tkinter import *
 
 # Creamos la raiz de nuestra aplicación
-'''root=Tk()
-root.geometry('560x690')
-root.config(bg='grey')'''
 habitacion = Tk()
 habitacion.geometry('500x630')
 habitacion.config(borderwidth=30)
 habitacion.config(relief="solid")
 habitacion.config(bg="white")
-#habitacion.pack(side=TOP)
-mueble = Frame(habitacion, width=90,height=260)
-mueble.config(bg='lightblue')
+texto1=Label(habitacion, text="Zona 1")
+texto1.pack(side=TOP)
+texto2=Label(habitacion, text="Zona 2")
+texto2.pack(side=LEFT)
+texto3=Label(habitacion, text="Zona 3")
+texto3.pack(side=RIGHT)
+texto4=Label(habitacion, text="Zona 4")
+texto4.pack(side=BOTTOM)
+mueble = Frame(habitacion, width=90,height=1)
+mueble.config(bg='white')
 mueble.place(width=146,height=350)
 mueble.pack(side=LEFT)
-mueble.pack()
+mueble1= Frame(habitacion, width=90,height=260)
+mueble1.config(bg='brown')
+mueble1.pack(side=LEFT)
 
-
-"""#divido la habitacion en zonas
-
-zona1 = {"largo":500,"ancho":150}
-zona2 = {"largo":480,"ancho":101}
-zona3 = {"largo":309,"ancho":480}
-zona4 = {"largo":90,"ancho":220}
-
-#creo una lista con las zonas para tener la zona total
-zona_total = []
-zona_total.append(zona1)
-zona_total.append(zona2)
-zona_total.append(zona3)
-zona_total.append(zona4)
-
-ventana= Frame( width=2000, height=240)"""
 habitacion.mainloop()
